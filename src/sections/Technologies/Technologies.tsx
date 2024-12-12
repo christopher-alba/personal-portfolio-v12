7e64915544e480123e5c8457c4c29d3daaf4f470
Christopher Sy Alba <christopher.alba@turners.co.nz>
2024-12-12 22:14:02 +1300

    complete technology section

--- src/sections/Technologies/Technologies.tsx
+++ src/sections/Technologies/Technologies.tsx
@@ -1,35 +1,60 @@
 import { FC } from "react";
 import PieChart from "./components/DynamicPieChart/DynamicPieChart";
-import { chartData } from "./data";
+import { barGraphData, chartData } from "./data";
 import Container from "../../components/Container/Container";
 import "./technologies.scss";
 
 const Technologies: FC<{ themeName: string }> = ({ themeName }) => {
   return (
-    <div id="technology-main-div">
+    <div className="technology-main-div">
       <Container>
-        <h1>Technologies</h1>
-        <div id="technology-upper-div">
-          <div id="technology-content-div">
-            <p>
-              I’m a full stack software engineer, so{" "}
-              <strong>
-                my skills range from frontend to backend technologies.
-              </strong>{" "}
-              I have more professional experience developing enterprise level
-              backend solutions, but spend most of my own time outside of work
-              developing in the frontend.
-            </p>
-            <h1>Comfort Levels</h1>
-            <p>
-              The graph indicates the different technologies I've had experience
-              in and my level of comfort in using them. This is by no way a
-              representation of how skilled I am at these technologies compared
-              to myself or other people.
-            </p>
-          </div>
-          <div id="technology-graphic-div">
+        <div className="technology-heading">
+          <h1 className="technology-title heading">TECHNOLOGIES</h1>
+          <h1 className="technology-subtitle heading">
+            <span style={{ whiteSpace: "nowrap" }}>COMFORT +</span> USAGE
+          </h1>
+        </div>
+        <p>
+          My skills range from frontend to backend technologies. I have more
+          professional experience developing enterprise level backend solutions,
+          but spend most of my own time outside of work developing in the
+          frontend.
+        </p>
+        <div className="flex-wrapper">
+          <div className="technology-chart-wrapper">
+            <p style={{ position: "absolute" }}>Figure 1.</p>
             <PieChart data={chartData} themeName={themeName} />
+          </div>
+          <div className="technology-description-wrapper">
+            <h1>Comfort Levels Graph</h1>
+            <p>
+              Figure 1 shows how comfortable I am with certain technologies. It
+              DOES NOT represent my skill levels or how I compare to other
+              engineers.
+            </p>
+            <br />
+            <hr />
+            <br />
+            <p>Figure 2.</p>
+            <h1>My Most Used Technologies.</h1>
+            {barGraphData
+              .sort((a, b) => b.value - a.value)
+              .map((tech) => {
+                return (
+                  <div style={{ display: "flex", width: "100%", alignItems:"center" }}>
+                    <p style={{fontSize: "0.7rem", width: "6rem", textAlign: "right", whiteSpace: "nowrap", marginRight: "10px"}}>{tech.label}</p>
+                    <div style={{ height: "10px", width: "100%" }}>
+                      <div
+                        style={{
+                          width: (tech.value / 10) * 100 + "%",
+                          height: "10px",
+                          background: "var(--blue-main)",
+                        }}
+                      ></div>
+                    </div>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </Container>

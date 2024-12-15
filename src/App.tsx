dbf21ed78b2a0f73aec6f3db38d31c8f4335c3bc
Christopher Alba <christopher.alba@turners.co.nz>
2024-12-16 09:23:36 +1300

    make fixes to ui

--- src/App.tsx
+++ src/App.tsx
@@ -14,132 +14,137 @@ import About from "./sections/About/About";
 import { Physics } from "@react-three/cannon";
 
 const Camera = () => {
-  const { camera } = useThree();
-  const [scrollPos, setScrollPos] = useState(0);
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
-  // Handle scroll event to update scroll position
-  const onScroll = () => {
-    const scrollValue = window.scrollY; // Get the current scroll position
-    setScrollPos(scrollValue);
-  };
+	const { camera } = useThree();
+	const [scrollPos, setScrollPos] = useState(0);
+	const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+	// Handle scroll event to update scroll position
+	const onScroll = () => {
+		const scrollValue = window.scrollY; // Get the current scroll position
+		setScrollPos(scrollValue);
+	};
 
-  // Handle mouse movement to update mouse position
-  const onMouseMove = (event: MouseEvent) => {
-    const x = (event.clientX / window.innerWidth) * 2 - 1; // Normalize to [-1, 1]
-    const y = -(event.clientY / window.innerHeight) * 2 + 1; // Normalize to [-1, 1]
-    setMousePos({ x, y });
-  };
+	// Handle mouse movement to update mouse position
+	const onMouseMove = (event: MouseEvent) => {
+		const x = (event.clientX / window.innerWidth) * 2 - 1; // Normalize to [-1, 1]
+		const y = -(event.clientY / window.innerHeight) * 2 + 1; // Normalize to [-1, 1]
+		setMousePos({ x, y });
+	};
 
-  // Attach scroll event listener
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-    window.addEventListener("mousemove", onMouseMove);
-    return () => {
-      window.removeEventListener("scroll", onScroll);
-      window.removeEventListener("mousemove", onMouseMove);
-    };
-  }, []);
+	// Attach scroll event listener
+	useEffect(() => {
+		window.addEventListener("scroll", onScroll);
+		window.addEventListener("mousemove", onMouseMove);
+		return () => {
+			window.removeEventListener("scroll", onScroll);
+			window.removeEventListener("mousemove", onMouseMove);
+		};
+	}, []);
 
-  // Set the initial camera position
-  useEffect(() => {
-    camera.position.z = 10; // Set the initial Z position
-  }, [camera]);
+	// Set the initial camera position
+	useEffect(() => {
+		camera.position.z = 10; // Set the initial Z position
+	}, [camera]);
 
-  // Move the camera based on the scroll position
-  useEffect(() => {
-    // Adjust how fast the camera moves based on the scroll position
-    const speed = 0.01;
-    const movement = scrollPos * speed;
+	// Move the camera based on the scroll position
+	useEffect(() => {
+		// Adjust how fast the camera moves based on the scroll position
+		const speed = 0.01;
+		const movement = scrollPos * speed;
 
-    // Move the camera along the Z-axis or along a path based on the scroll position
-    camera.position.z = 20 - movement; // You can modify the starting position and direction of movement
+		// Move the camera along the Z-axis or along a path based on the scroll position
+		camera.position.z = 20 - movement; // You can modify the starting position and direction of movement
 
-    const rotationSpeed = -0.125; // Rotation sensitivity
-    camera.rotation.x = mousePos.y * rotationSpeed; // Rotate along X-axis based on mouse Y
-    camera.rotation.y = mousePos.x * rotationSpeed; // Rotate along Y-axis based on mouse X
+		const rotationSpeed = -0.125; // Rotation sensitivity
+		camera.rotation.x = mousePos.y * rotationSpeed; // Rotate along X-axis based on mouse Y
+		camera.rotation.y = mousePos.x * rotationSpeed; // Rotate along Y-axis based on mouse X
 
-    // Optionally, you can add lateral movement (X or Y) to create a more dynamic effect
+		// Optionally, you can add lateral movement (X or Y) to create a more dynamic effect
 
-    // Ensure the camera always looks at the center of the scene (optional)
-  }, [scrollPos, camera, mousePos]);
+		// Ensure the camera always looks at the center of the scene (optional)
+	}, [scrollPos, camera, mousePos]);
 
-  return null;
+	return null;
 };
 
 function App() {
-  const [theme, setTheme] = useState(
-    localStorage.getItem("theme") === "light-theme"
-      ? "light-theme"
-      : "dark-theme"
-  );
-  useEffect(() => {
-    Aos.init();
-  });
+	const [theme, setTheme] = useState(
+		localStorage.getItem("theme") === "light-theme"
+			? "light-theme"
+			: "dark-theme"
+	);
+	useEffect(() => {
+		Aos.init();
+	});
 
-  const getColorFromTheme = (theme: string) => {
-    let color =
-      theme === "light-theme"
-        ? `hsl(220, ${Math.floor(Math.random() * 20 + 80)}%, ${Math.floor(
-            Math.random() * 20 + 80
-          )}%)` // Hue = 200 (blue), Saturation = 80-100%, Lightness = 80-100%
-        : "black";
-    console.log(color);
-    return color;
-  };
-  // Map 100 Rooms
-  const rooms = Array.from({ length: 1000 }, (_, index) => {
-    const xPosition = (Math.random() - 0.5) * 50; // Random X position between -25 and 25
-    const yPosition = (Math.random() - 0.5) * 50; // Random Y position between 0 and 10
-    const zPosition = (Math.random() - 0.5) * 150; // Random Z position between -25 and 25
+	const getColorFromTheme = (theme: string) => {
+		let color =
+			theme === "light-theme"
+				? `hsl(220, ${Math.floor(
+						Math.random() * 20 + 80
+				  )}%, ${Math.floor(Math.random() * 20 + 80)}%)` // Hue = 200 (blue), Saturation = 80-100%, Lightness = 80-100%
+				: "black";
+		console.log(color);
+		return color;
+	};
+	// Map 100 Rooms
+	const rooms = Array.from({ length: 1000 }, (_, index) => {
+		const xPosition = (Math.random() - 0.5) * 50; // Random X position between -25 and 25
+		const yPosition = (Math.random() - 0.5) * 50; // Random Y position between 0 and 10
+		const zPosition = (Math.random() - 0.5) * 150; // Random Z position between -25 and 25
 
-    return (
-      <Room
-        key={index}
-        position={[xPosition, yPosition, zPosition]} // Random position
-        theme={getColorFromTheme(theme)}
-      />
-    );
-  });
+		return (
+			<Room
+				key={index}
+				position={[xPosition, yPosition, zPosition]} // Random position
+				theme={getColorFromTheme(theme)}
+			/>
+		);
+	});
 
-  return (
-    <>
-      <div className={theme}>
-        <Navbar theme={theme} setTheme={setTheme} />
-        <Canvas style={{ position: "fixed", zIndex: 0 }}>
-          <Camera />
-          <ambientLight
-            intensity={(Math.PI / 2) * (theme === "light-theme" ? 10 : 1)}
-          />
-          <spotLight
-            position={[10, 10, 1000]}
-            angle={0.15}
-            penumbra={1}
-            decay={0}
-            intensity={Math.PI}
-          />
-          <pointLight
-            position={[-10, -10, -1000]}
-            decay={0}
-            intensity={Math.PI}
-          />
-          <Physics gravity={[0, 0, 0]}>{rooms}</Physics>
-        </Canvas>
-        <div style={{ zIndex: 1, position: "relative" }}>
-          <Landing />
-          <div className="hide-3d">
-            <About />
-            <Skills />
-          </div>
-          <Technologies themeName={theme} />
-          <Career />
-          <div className="hide-3d">
-            <Education />
-          </div>
-          <Footer />
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={theme}>
+				<Navbar
+					theme={theme}
+					setTheme={setTheme}
+				/>
+				<Canvas style={{ position: "fixed", zIndex: 0 }}>
+					<Camera />
+					<ambientLight
+						intensity={
+							(Math.PI / 2) * (theme === "light-theme" ? 10 : 1)
+						}
+					/>
+					<spotLight
+						position={[10, 10, 1000]}
+						angle={0.15}
+						penumbra={1}
+						decay={0}
+						intensity={Math.PI}
+					/>
+					<pointLight
+						position={[-10, -10, -1000]}
+						decay={0}
+						intensity={Math.PI}
+					/>
+					<Physics gravity={[0, 0, 0]}>{rooms}</Physics>
+				</Canvas>
+				<div style={{ zIndex: 1, position: "relative" }}>
+					<Landing />
+					<div className="fade-in-section">
+						<About />
+					</div>
+					<div className="hide-3d">
+						<Skills />
+						<Technologies themeName={theme} />
+						<Career />
+						<Education />
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/Room/Room.tsx
+++ src/components/Room/Room.tsx
@@ -3,64 +3,67 @@ import { useFrame } from "@react-three/fiber";
 import { useSphere } from "@react-three/cannon"; // Hook to create a spherical collision shape
 
 function Box(props: any) {
-  const meshRef = useRef() as any; // Reference to the mesh for rotation
+	const meshRef = useRef() as any; // Reference to the mesh for rotation
 
-  // Random rotation speeds for X, Y, and Z axes
-  const [rotationSpeed] = useState({
-    x: Math.random() * 0.5, // Random speed for X axis rotation
-    y: Math.random() * 0.5, // Random speed for Y axis rotation
-    z: Math.random() * 0.5, // Random speed for Z axis rotation
-  });
+	// Random rotation speeds for X, Y, and Z axes
+	const [rotationSpeed] = useState({
+		x: Math.random() * 0.5, // Random speed for X axis rotation
+		y: Math.random() * 0.5, // Random speed for Y axis rotation
+		z: Math.random() * 0.5, // Random speed for Z axis rotation
+	});
 
-  // Create a physical body for the box using useSphere (can use useBox for a box-shaped object)
-  const [ref] = useSphere(() => ({
-    mass: 1, // Set the mass (affects how the object interacts with others)
-    position: [props.position[0], props.position[1], props.position[2]],
-    args: [1], // Size of the sphere (radius)
-    friction: 0.5, // Friction coefficient
-    restitution: 0.7, // Bounciness (for collisions)
-  })) as any;
+	// Create a physical body for the box using useSphere (can use useBox for a box-shaped object)
+	const [ref] = useSphere(() => ({
+		mass: 1, // Set the mass (affects how the object interacts with others)
+		position: [props.position[0], props.position[1], props.position[2]],
+		args: [1], // Size of the sphere (radius)
+		friction: 0.5, // Friction coefficient
+		restitution: 0.7, // Bounciness (for collisions)
+	})) as any;
 
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((_, delta) => {
-    if (meshRef.current) {
-      // Applying rotation to mesh while respecting physics collision
-      meshRef.current.rotation.x += rotationSpeed.x * delta;
-      meshRef.current.rotation.y += rotationSpeed.y * delta;
-      meshRef.current.rotation.z += rotationSpeed.z * delta;
-    }
+	// Subscribe this component to the render-loop, rotate the mesh every frame
+	useFrame((_, delta) => {
+		if (meshRef.current) {
+			// Applying rotation to mesh while respecting physics collision
+			meshRef.current.rotation.x += rotationSpeed.x * delta;
+			meshRef.current.rotation.y += rotationSpeed.y * delta;
+			meshRef.current.rotation.z += rotationSpeed.z * delta;
+		}
 
-    // Keep the physics body position synced with mesh
-    if (ref.current) {
-      ref.current.position.set(
-        meshRef.current.position.x,
-        meshRef.current.position.y,
-        meshRef.current.position.z
-      );
-    }
-  });
-  const randomNumber = Math.random();
-  // Return view, these are regular three.js elements expressed in JSX
-  console.log(props.theme);
+		// Keep the physics body position synced with mesh
+		if (ref.current) {
+			ref.current.position.set(
+				meshRef.current.position.x,
+				meshRef.current.position.y,
+				meshRef.current.position.z
+			);
+		}
+	});
+	const randomNumber = Math.random();
+	// Return view, these are regular three.js elements expressed in JSX
+	console.log(props.theme);
 
-  return (
-    <mesh ref={meshRef} {...props}>
-      {randomNumber < 0.3 ? (
-        <octahedronGeometry args={[2 * Math.random()]} />
-      ) : randomNumber < 0.6 ? (
-        <boxGeometry args={[2 * Math.random()]} />
-      ) : (
-        <sphereGeometry args={[2 * Math.random(), 5, 5]} />
-      )}
+	return (
+		<mesh
+			ref={meshRef}
+			{...props}
+		>
+			{randomNumber < 0.3 ? (
+				<octahedronGeometry args={[2 * Math.random()]} />
+			) : randomNumber < 0.6 ? (
+				<boxGeometry args={[2 * Math.random()]} />
+			) : (
+				<sphereGeometry args={[2 * Math.random(), 5, 5]} />
+			)}
 
-      <meshStandardMaterial
-        color={props.theme}
-        roughness={0.3}
-        metalness={0.6}
-        wireframe
-      />
-    </mesh>
-  );
+			<meshStandardMaterial
+				color={props.theme}
+				roughness={0.3}
+				metalness={0.7}
+				wireframe
+			/>
+		</mesh>
+	);
 }
 
 export default Box;
